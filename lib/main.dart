7ab7351c5fa911b47ee8dd693c7f83d243f34951
Muniappan <muniappan.subramanian@syncfusiopn.com>
2022-12-12 15:12:15 +0530

    sample updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-void main() => runApp(MultipleResource());
+void main() => runApp(const MultipleResource());
 
 class MultipleResource extends StatefulWidget {
+  const MultipleResource({super.key});
+
   @override
   MultipleResourceState createState() => MultipleResourceState();
 }
@@ -11,7 +13,6 @@ class MultipleResource extends StatefulWidget {
 class MultipleResourceState extends State<MultipleResource> {
   bool _isJoseph = false;
   bool _isStephen = false;
-  final List<Appointment> _appointments = <Appointment>[];
   final DataSource _dataSource=DataSource(<Appointment>[]);
   final List<Appointment> _josephAppointments=<Appointment>[];
   final List<Appointment> _stephenAppointments=<Appointment>[];
@@ -50,7 +51,7 @@ class MultipleResourceState extends State<MultipleResource> {
                       activeTrackColor: Colors.lightGreenAccent,
                       activeColor: Colors.green,
                     ),
-                    Text('Dr.Joseph (Nephrologist)'),
+                    const Text('Dr.Joseph (Nephrologist)'),
                   ],
                 ),
               ),
@@ -79,7 +80,7 @@ class MultipleResourceState extends State<MultipleResource> {
                     activeTrackColor: Colors.lightBlue,
                     activeColor: Colors.blue,
                   ),
-                  Text('Dr.Stephen (Cardiologist)'),
+                  const Text('Dr.Stephen (Cardiologist)'),
                 ],
               ),
               Expanded(
@@ -95,32 +96,32 @@ class MultipleResourceState extends State<MultipleResource> {
   void _updateJosephAppointments() {
     Appointment newAppointment = Appointment(
       startTime: DateTime.now(),
-      endTime: DateTime.now().add(Duration(hours: 1)),
+      endTime: DateTime.now().add(const Duration(hours: 1)),
       subject: 'Interactive Live Workshop on Robotic Surgery in Gynecology',
       color: Colors.lightGreen,
     );
     Appointment newAppointment1 = Appointment(
-      startTime: DateTime.now().add(Duration(days: -3, hours: 6)),
-      endTime: DateTime.now().add(Duration(days: -3, hours: 7)),
+      startTime: DateTime.now().add(const Duration(days: -3, hours: 6)),
+      endTime: DateTime.now().add(const Duration(days: -3, hours: 7)),
       subject:
       'Billion Hearts Beating brings you Happy Heart Fest to celebrate World Heart Day',
       color: Colors.lightGreen,
     );
     Appointment newAppointment2 = Appointment(
-      startTime: DateTime.now().add(Duration(days: -2, hours: 5)),
-      endTime: DateTime.now().add(Duration(days: -2, hours: 6)),
+      startTime: DateTime.now().add(const Duration(days: -2, hours: 5)),
+      endTime: DateTime.now().add(const Duration(days: -2, hours: 6)),
       subject: 'Join Hands for Patient Safety',
       color: Colors.lightGreen,
     );
     Appointment newAppointment3 = Appointment(
-      startTime: DateTime.now().add(Duration(days: -1, hours: 4)),
-      endTime: DateTime.now().add(Duration(days: -1, hours: 5)),
+      startTime: DateTime.now().add(const Duration(days: -1, hours: 4)),
+      endTime: DateTime.now().add(const Duration(days: -1, hours: 5)),
       subject: 'Robotic GI Surgery International Congress',
       color: Colors.lightGreen,
     );
     Appointment newAppointment4 = Appointment(
-      startTime: DateTime.now().add(Duration(days: 3, hours: 8)),
-      endTime: DateTime.now().add(Duration(days: 3, hours: 9)),
+      startTime: DateTime.now().add(const Duration(days: 3, hours: 8)),
+      endTime: DateTime.now().add(const Duration(days: 3, hours: 9)),
       subject: 'World Continence Week - Free Check-up Camp for women',
       color: Colors.lightGreen,
     );
@@ -134,31 +135,31 @@ class MultipleResourceState extends State<MultipleResource> {
   void _updateStephenAppointments() {
     Appointment newAppointment5 = Appointment(
       startTime: DateTime.now(),
-      endTime: DateTime.now().add(Duration(hours: 1)),
+      endTime: DateTime.now().add(const Duration(hours: 1)),
       subject: 'CME (Continuing Medical Education) Program',
       color: Colors.lightBlue,
     );
     Appointment newAppointment6 = Appointment(
-      startTime: DateTime.now().add(Duration(days: 1, hours: 4)),
-      endTime: DateTime.now().add(Duration(days: 1, hours: 5)),
+      startTime: DateTime.now().add(const Duration(days: 1, hours: 4)),
+      endTime: DateTime.now().add(const Duration(days: 1, hours: 5)),
       subject: '4th Clinical Nutrition Update',
       color: Colors.lightBlue,
     );
     Appointment newAppointment7 = Appointment(
-      startTime: DateTime.now().add(Duration(days: 2, hours: 3)),
-      endTime: DateTime.now().add(Duration(days: 2, hours: 4)),
+      startTime: DateTime.now().add(const Duration(days: 2, hours: 3)),
+      endTime: DateTime.now().add(const Duration(days: 2, hours: 4)),
       subject: 'National CME on Contemporary Developments in Transplants',
       color: Colors.lightBlue,
     );
     Appointment newAppointment8 = Appointment(
-      startTime: DateTime.now().add(Duration(days: 3, hours: 6)),
-      endTime: DateTime.now().add(Duration(days: 3, hours: 7)),
+      startTime: DateTime.now().add(const Duration(days: 3, hours: 6)),
+      endTime: DateTime.now().add(const Duration(days: 3, hours: 7)),
       subject: 'Oncological Robotic Surgery',
       color: Colors.lightBlue,
     );
     Appointment newAppointment9 = Appointment(
-      startTime: DateTime.now().add(Duration(days: -1, hours: 6)),
-      endTime: DateTime.now().add(Duration(days: -1, hours: 7)),
+      startTime: DateTime.now().add(const Duration(days: -1, hours: 6)),
+      endTime: DateTime.now().add(const Duration(days: -1, hours: 7)),
       subject: 'World Ostomy Day and Awareness Program',
       color: Colors.lightBlue,
     );
